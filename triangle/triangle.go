0d13a28ee7f40f4b9cbecf01720fd86c1c36d6fa
byarbrough <bcynmelk+git@gmail.com>
2020-11-20 23:02:02 -0500

    Add type definition

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -5,24 +5,23 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
-
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	NaT = -1 // not a triangle
+	Equ = 0  // equilateral
+	Iso = 1  // isosceles
+	Sca = 2  // scalene
 )
 
 // KindFromSides should have a comment documenting it.
-func KindFromSides(a, b, c float64) Kind {
+func KindFromSides(a, b, c float64) (k Kind) {
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-	var k Kind
+
 	return k
 }
