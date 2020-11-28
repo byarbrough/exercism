ef94f015c9953c33e071240b36d277c844cf0507
byarbrough <bcynmelk+git@gmail.com>
2020-11-28 13:02:46 -0500

    Compute seconds in map
    This improves the benchmark, since it only has to do the multiplication once.
    
    Also cleared up variable names and doc

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -4,23 +4,22 @@ package space
 // Planet is the name of a planet in our solar system
 type Planet string
 
-const secondsInYear = 31557600
+// earthYearSecs is how many seconds are in one Earth year
+const earthYearSecs = 31557600.0
 
-var relativePeriod = map[Planet]float64{
-	"Mercury": 0.2408467,
-	"Venus":   0.61519726,
-	"Earth":   1.0,
-	"Mars":    1.8808158,
-	"Jupiter": 11.862615,
-	"Saturn":  29.447498,
-	"Uranus":  84.016846,
-	"Neptune": 164.79132,
+// PlanetYearSecs maps number seconds in a planet's year
+var PlanetYearSecs = map[Planet]float64{
+	"Mercury": earthYearSecs * 0.2408467,
+	"Venus":   earthYearSecs * 0.61519726,
+	"Earth":   earthYearSecs * 1.0,
+	"Mars":    earthYearSecs * 1.8808158,
+	"Jupiter": earthYearSecs * 11.862615,
+	"Saturn":  earthYearSecs * 29.447498,
+	"Uranus":  earthYearSecs * 84.016846,
+	"Neptune": earthYearSecs * 164.79132,
 }
 
 // Age returns the age of someone in Earth Years, given their time on another planet
 func Age(seconds float64, planet Planet) (age float64) {
-	// Convert seconds to years
-	years := seconds / secondsInYear
-
-	return years / relativePeriod[planet]
+	return seconds / PlanetYearSecs[planet]
 }
