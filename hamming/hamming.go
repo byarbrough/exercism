a6e877f553289d5ea0627c9e04e312d53acb5345
byarbrough <bcynmelk+git@gmail.com>
2021-02-20 13:26:44 -0500

    Computes Hamming Distance

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,21 @@
 package hamming
 
+import "fmt"
+
+// Distance computes the Hamming Distance for two strings
 func Distance(a, b string) (int, error) {
-	return 0, nil
+	// strings must be equal length
+	if len(a) != len(b) {
+		return 0, fmt.Errorf("%s and %s unequal length", a, b)
+	}
+
+	distance := 0
+	// iterate over strings
+	for pos, char := range a {
+		if char != rune(b[pos]) {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
